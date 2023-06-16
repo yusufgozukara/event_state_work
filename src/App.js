4d2b4f3ca26e135b0f9784f8c11f8f077bfff865
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-06-16 11:28:42 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Events from "./components/1-event-intro/Events";
+import Counter from "./components/2-classComponents/Counter";
+import UseStateExample from "./components/3-useStateHook/UseStateExample";
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
+export default App;
